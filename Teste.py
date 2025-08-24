9eb5e9c1325ffc474dd3d4e3458f64db40e9e999
RafaNaka33 <rafael.kazuo.tn@gmail.com>
2025-08-24 16:05:24 -0300

    Add files via upload
    precisa das bibliotecas/comandos "pip install matplotlib" e "pip install numpy" para o python ou com o acréscimo do "!" antes de "pip" para plataformas como o google Colab

---
+++ Teste.py
@@ -0,0 +1,32 @@
+ano = "2025"
+trimestre = "3° Trimestre"
+print(f"{ano}, {trimestre} Primeiro teste")
+import matplotlib.pyplot as plt
+import numpy as np
+import math as mt
+
+x1 = [0, 1, 2, 3, 4, 5]
+y1=[i**2 for i in x1]
+
+x2 = [0, 1, 2, 3, 4, 5]
+y2 = [i*2 for i in x2]
+
+x3 = [0, 1, 2, 3, 4, 5]
+k = 5
+y3 = [(k*i**2)/2 for i in x3]
+
+x4 = [0, 1, 2, 3, 4, 5]
+y4 = [(mt.exp(i))/2 for i in x4]
+
+plt.plot(x1, y1, label='linha y=x²', color='black', marker='*')
+plt.plot(x2, y2, label='linha y=2x', color='green', marker='^')
+plt.plot(x3, y3, label='linha y=(k*x^2)/2', color='red', marker='o')
+plt.plot(x4, y4, label='linha y=e^x/2', color='orange', marker='s')
+
+plt.title("exemplo de gráfico")
+plt.xlabel("eixo x")
+plt.ylabel("eixo y")
+plt.legend()
+plt.grid(True, color='blue', linestyle='--')
+
+plt.show()
