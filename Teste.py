a298dbdd952a83964bbd48457e919c93e294617c
RafaNaka33 <rafael.kazuo.tn@gmail.com>
2025-08-24 17:03:31 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -18,10 +18,16 @@ y3 = [(k*i**2)/2 for i in x3]
 x4 = [0, 1, 2, 3, 4, 5]
 y4 = [(mt.exp(i))/2 for i in x4]
 
+x5 = np.linspace(0, 2*np.pi, 100)
+y_seno = np.sin(x5)
+y_cosseno = np.cos(x5)
+
 plt.plot(x1, y1, label='linha y=x²', color='black', marker='*')
 plt.plot(x2, y2, label='linha y=2x', color='green', marker='^')
 plt.plot(x3, y3, label='linha y=(k*x^2)/2', color='red', marker='o')
 plt.plot(x4, y4, label='linha y=e^x/2', color='orange', marker='s')
+plt.plot(x5, y_seno, label='Seno')
+plt.plot(x5, y_cosseno, label='Cos')
 
 plt.title("exemplo de gráfico")
 plt.xlabel("eixo x")
